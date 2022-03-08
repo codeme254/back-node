d27329a4eef5136272e34ad29733ac5e0242e9a6
otwoma <otwomadennis365@gmail.com>
2022-03-08 17:34:09 +0300

    started the barebones of node

---
+++ node 1/index.js
@@ -0,0 +1,10 @@
+// node releases
+// even numbered versions of node currently are or will eventually become an LTS version
+// odd numbered versions will only be current versions. they will never have the stability that lts versions give us
+// each lts version is seperated into an active phase and a maintainance phase, when active most changes are accepted as long as they don't break existing codes.
+// go with even numbered of versions if you will deploy your app in a few.
+// REPL: read, eval print and loop
+// Reading in the code we type and parsing the code. V8 breaking down the code and understanding the role of everythin in the code
+// Eval: evaluating the code
+// Printing the output
+// Loop: going back
